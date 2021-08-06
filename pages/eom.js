31fce9095d69ec439f62ac18aa607e5ca23dece9
Fatih Durdu <mehmet-fatih.durdu@sovos.com>
2021-08-06 11:39:35 +0300

    Completed news app

---
+++ pages/eom.js
@@ -0,0 +1,39 @@
+import { Toolbar } from '../components/toolbar';
+import styles from '../styles/eom.module.css';
+
+function eom({ employee }) {
+
+    console.log(employee)
+    return (
+        <div className="page-container">
+            <Toolbar></Toolbar>
+            <div className={styles.main}>
+                <h1>
+                    Emplyee Of The Month
+                </h1>
+            </div>
+            <div className={styles.employeeOfMonth}>
+                <h3>{employee.name}</h3>
+                <h6>{employee.position}</h6>
+                <img src={employee.image}></img>
+                <p>{employee.description}</p>
+            </div>
+        </div>
+
+    )
+}
+
+export const getServerSideProps = async pageContext => {
+
+    const apiResponse = await fetch('https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth')
+
+    const employee = await apiResponse.json();
+
+    return {
+        props: {
+            employee: employee
+        }
+    }
+}
+
+export default eom
